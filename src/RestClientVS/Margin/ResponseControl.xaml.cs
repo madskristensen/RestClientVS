a6dd6fc9c50f68fc18ac1439e3c89a61691e9fca
Mister Magoo <mister.magoo+githubsql@gmail.com>
2022-02-03 18:16:12 +0000

    Switch to using a partial match to cath more Media Types

--- src/RestClientVS/Margin/ResponseControl.xaml.cs
+++ src/RestClientVS/Margin/ResponseControl.xaml.cs
@@ -18,42 +18,42 @@ namespace RestClientVS.Margin
             if (result.Response != null)
             {
                 var text = new StringBuilder();
-                switch (result.Response.Content.Headers.ContentType.MediaType)
+                var mediaType = result.Response.Content.Headers.ContentType.MediaType;
+                if (mediaType.IndexOf("json", StringComparison.OrdinalIgnoreCase) > -1)
                 {
-                    case "application/json":
-                        var jsonString = await result.Response.Content.ReadAsStringAsync();
-                        text.AppendLine(result.Response.Headers.ToString());
-                        try
-                        {
-                            var jsonObject = JObject.Parse(jsonString);
-                            text.AppendLine(jsonObject.ToString());
-                        }
-                        catch (Exception ex)
-                        {
-                            text.AppendFormat("** {0} : Error parsing JSON ( {1} ), raw content follows. **\r\r", nameof(RestClientVS), ex.GetBaseException().Message);
-                            text.AppendLine(jsonString);
-                        }
-                        Control.Text = text.ToString();
-                        break;
-                    case "application/xml":
-                        var xmlString = await result.Response.Content.ReadAsStringAsync();
-                        text.AppendLine(result.Response.Headers.ToString());
-                        try
-                        {
-                            var xmlElement = XElement.Parse(xmlString);
-                            text.AppendLine(xmlElement.ToString());
-                        }
-                        catch (Exception ex)
-                        {
-                            text.AppendFormat("** {0} : Error parsing XML ( {1} ), raw content follows. **\r\r", nameof(RestClientVS), ex.GetBaseException().Message);
-                            text.AppendLine(xmlString);
-                        }
-                        Control.Text = text.ToString();
-                        break;
-                    default:
-                        Control.Text = await result.Response.ToRawStringAsync();
-                        break;
+                    var jsonString = await result.Response.Content.ReadAsStringAsync();
+                    text.AppendLine(result.Response.Headers.ToString());
+                    try
+                    {
+                        var jsonObject = JObject.Parse(jsonString);
+                        text.AppendLine(jsonObject.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        text.AppendFormat("** {0} : Error parsing JSON ( {1} ), raw content follows. **\r\r", nameof(RestClientVS), ex.GetBaseException().Message);
+                        text.AppendLine(jsonString);
+                    }
+                    Control.Text = text.ToString();
+                    return;
                 }
+                if (mediaType.IndexOf("xml", StringComparison.OrdinalIgnoreCase) > -1)
+                {
+                    var xmlString = await result.Response.Content.ReadAsStringAsync();
+                    text.AppendLine(result.Response.Headers.ToString());
+                    try
+                    {
+                        var xmlElement = XElement.Parse(xmlString);
+                        text.AppendLine(xmlElement.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        text.AppendFormat("** {0} : Error parsing XML ( {1} ), raw content follows. **\r\r", nameof(RestClientVS), ex.GetBaseException().Message);
+                        text.AppendLine(xmlString);
+                    }
+                    Control.Text = text.ToString();
+                    return;
+                }
+                Control.Text = await result.Response.ToRawStringAsync();
             }
             else
             {
