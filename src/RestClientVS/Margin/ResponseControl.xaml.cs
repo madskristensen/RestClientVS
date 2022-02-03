c2693597241c66703cc594131a235d7634deef29
Mister Magoo <mister.magoo+githubsql@gmail.com>
2022-02-03 15:07:05 +0000

    Add basic formatting for JSON and XML results

--- src/RestClientVS/Margin/ResponseControl.xaml.cs
+++ src/RestClientVS/Margin/ResponseControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Text;
+using System.Windows.Controls;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
 using RestClient.Client;
 
 namespace RestClientVS.Margin
@@ -14,7 +17,43 @@ namespace RestClientVS.Margin
         {
             if (result.Response != null)
             {
-                Control.Text = await result.Response.ToRawStringAsync();
+                var text = new StringBuilder();
+                switch (result.Response.Content.Headers.ContentType.MediaType)
+                {
+                    case "application/json":
+                        var jsonString = await result.Response.Content.ReadAsStringAsync();
+                        text.AppendLine(result.Response.Headers.ToString());
+                        try
+                        {
+                            var jsonObject = JObject.Parse(jsonString);
+                            text.AppendLine(jsonObject.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            text.AppendFormat("** {0} : Error parsing JSON ( {1} ), raw content follows. **\r\r", nameof(RestClientVS), ex.GetBaseException().Message);
+                            text.AppendLine(jsonString);
+                        }
+                        Control.Text = text.ToString();
+                        break;
+                    case "application/xml":
+                        var xmlString = await result.Response.Content.ReadAsStringAsync();
+                        text.AppendLine(result.Response.Headers.ToString());
+                        try
+                        {
+                            var xmlElement = XElement.Parse(xmlString);
+                            text.AppendLine(xmlElement.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            text.AppendFormat("** {0} : Error parsing XML ( {1} ), raw content follows. **\r\r", nameof(RestClientVS), ex.GetBaseException().Message);
+                            text.AppendLine(xmlString);
+                        }
+                        Control.Text = text.ToString();
+                        break;
+                    default:
+                        Control.Text = await result.Response.ToRawStringAsync();
+                        break;
+                }
             }
             else
             {
